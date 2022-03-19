665ae1c6cdf9bb2446006b517a96e051e6f0e148
YEAN SEHA <seyha@sala.co>
2022-03-19 13:16:04 +0700

    50-Outputting Widget Conditionally

--- lib/main.dart
+++ lib/main.dart
@@ -19,18 +19,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int _questionIndex = 0;
 
-
-  void _answerQuestion(){
-    setState( (){
-      _questionIndex = _questionIndex + 1;
-    });
-    
-    print(_questionIndex);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    var questions = [
+    final questions = const [
       {'questionText': 'what is your favorite color?', 'answers': ['REd', 'Blue', 'Red'],
         
       },
@@ -42,19 +31,42 @@ class _MyAppState extends State<MyApp> {
       },
 
     ];
+
+  void _answerQuestion(){
+
+    if(_questionIndex < questions.length){
+      print('We have more questons !');
+    }else{
+      print('No more questions!');
+    }
+
+    setState( (){
+      _questionIndex = _questionIndex + 1;
+    });
+    
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+/*     questions = []; */
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first Flutter'),
         ),
-        body: Column(
+        body: _questionIndex < questions.length ? Column(
           children: [
             Question(questions[_questionIndex]['questionText'] as String),
             ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
               return Answer(_answerQuestion, answer);
             }).toList()
           ],
-        ),
+        ) : Center(
+          child: Text('I did it !'),
+        ) ,
       ),
     );
   }
